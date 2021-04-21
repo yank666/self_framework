bf5684922854064b9ccf3fdf91dd931fcff48d4a
yankai.yan <yankai.yan@cainiao.com>
2021-04-21 15:18:25 +0800

    for test

--- cmake_dependency/SuperBuild.cmake
+++ cmake_dependency/SuperBuild.cmake
@@ -1,15 +1,17 @@
 include (ExternalProject)
 
-ExternalProject_Add (External_glog
-        SOURCE_DIR ${PROJECT_SOURCE_DIR}/thirdparty/glog
-        CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-        -DBUILD_SHARED_LIBS=ON -DWITH_GFLAGS=OFF
-        INSTALL_COMMAND ""
-        BINARY_DIR ${PROJECT_SOURCE_DIR}/build/thirdparty/glog
-        )
-add_library(glog::glog STATIC IMPORTED)
-set_target_properties(glog::glog PROPERTIES IMPORTED_LOCATION ${PROJECT_SOURCE_DIR}/build/thirdparty/glog/libglog.so)
-add_dependencies(glog::glog External_glog)
+#ExternalProject_Add (External_glog
+#        SOURCE_DIR ${PROJECT_SOURCE_DIR}/thirdparty/glog
+#        CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+#        -DBUILD_SHARED_LIBS=ON -DWITH_GFLAGS=OFF
+#        INSTALL_COMMAND ""
+#        BINARY_DIR ${PROJECT_SOURCE_DIR}/build/thirdparty/glog
+#        )
+#add_library(glog::glog STATIC IMPORTED)
+#set_target_properties(glog::glog PROPERTIES IMPORTED_LOCATION ${PROJECT_SOURCE_DIR}/build/thirdparty/glog/libglog.so)
+#add_dependencies(glog::glog External_glog)
+#include_directories(${PROJECT_SOURCE_DIR}/build/thirdparty/glog)
+#include_directories(${PROJECT_SOURCE_DIR}/thirdparty/glog/src)
 
 ExternalProject_Add (OpenCV
         SOURCE_DIR ${PROJECT_SOURCE_DIR}/thirdparty/opencv
